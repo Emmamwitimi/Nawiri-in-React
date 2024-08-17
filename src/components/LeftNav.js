bb1980b780625a01ed93a183916ed1b699a5f6ab
Emmaculate <emmamwitimi14@gmail.com>
2024-08-17 20:25:21 +0300

    routing

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './LeftNav.css';
 
 const LeftNav = () => {
     return (
-        <div className="left-nav">
-            <ul>
-                <li>Home</li>
-                <li>Profile</li>
-                <li>Resources</li>
-                <li>Activities</li>
-            </ul>
-        </div>
+        <nav className="left-nav">
+            <NavLink to="/" className="nav-link" activeClassName="active">
+                Home
+            </NavLink>
+            <NavLink to="/profile" className="nav-link" activeClassName="active">
+                Profile
+            </NavLink>
+            <NavLink to="/activities" className="nav-link" activeClassName="active">
+                Activities
+            </NavLink>
+            <NavLink to="/resources" className="nav-link" activeClassName="active">
+                Resources
+            </NavLink>
+        </nav>
     );
 };
 
---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,9 @@
+import LeftNav from "../components/LeftNav";
+import React from "react";
+
+function ProfilePage(){
+    return(
+        <div></div>
+    )
+}
+export default ProfilePage;
