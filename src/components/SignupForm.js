ee84acb448f0b9f3126f8d6ad7007f98dea43a6d
Emmaculate <emmamwitimi14@gmail.com>
2024-08-18 13:34:32 +0300

    Auth

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { auth } from './firebaseConfig';
+import { auth } from '../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Signup = ({ onLoginClick }) => {
